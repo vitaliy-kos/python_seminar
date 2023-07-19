f1de4574f7cfdbfa71a78da934869fff9e061856
Vitaliy <kv1993@me.com>
2023-07-19 16:48:01 +0300

    Added decision to Tasks12

--- Task12-homework/main.py
+++ Task12-homework/main.py
@@ -2,3 +2,20 @@
 # Петя помогает Кате по математике. Он задумывает два натуральных числа X и Y (X,Y≤1000), 
 # а Катя должна их отгадать. Для этого Петя делает две подсказки. Он называет сумму 
 # этих чисел S и их произведение P. Помогите Кате отгадать задуманные Петей числа.
+
+sum = int(input("Insert sum of numbers: "))
+product = int(input("Insert product of numbers: "))
+
+first_num = 0
+second_num = 0
+
+for x in range(sum):
+    for y in range(product):
+        if sum == x + y and product == x * y:
+            first_num = x
+            second_num = y
+
+if first_num > 0:
+    print(f'first number = {first_num}, second number = {second_num} ')
+else:
+    print(f'for theese combination not exist numbers')
